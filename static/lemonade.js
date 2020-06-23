311b734fd6d11254741c1dfb95de286dea5fbd8d
Josh Collins <jcollins@pivotal.io>
2020-06-23 02:15:02 -0700

    customers sad when lemonade bad

--- static/lemonade.js
+++ static/lemonade.js
@@ -2,60 +2,12 @@ lemonadeColor=""
 lastCount=0
 counter=0
 currentNumber=0
+lastActionNumber=0
+minCount=0
 
 
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
-}
-
-async function showCustomers(number) {
-  if(number > 0){
-    await sleep(5000)
-  }
-  for(i = 1; i <= 8; i++) {
-    document.querySelector("#customer" + i).style.display = "none"
-    document.querySelector("#sad-customer-face-" + i).style.display = "none"
-  }
-
-
-  for(i = 1; i <= number; i++) {
-    if(number >= 2){
-      try {
-        document.querySelector("#customer" + i).style.display = "inline"
-        if(lemonadeColor == "new-recipe"){
-          document.querySelector("#sad-customer-face-" + i).style.display = "inline"
-        }
-      }
-      catch(err) {
-        console.log("showCustomers", err)
-      }
-    }
-  }
-
-}
-
-function showSigns(number) {
-  if(number > 0) {
-    document.querySelector("#city_map").style.display = "inline"
-  } else {
-    document.querySelector("#city_map").style.display = "none"
-  }
-
-  document.querySelector("#lemonade_signs").style.display = "inline"
-
-  for(i = 1; i <= 8; i++) {
-    document.querySelector("#lemonade_sign" + i).style.display = "none"
-  }
-
-  for(i = 1; i <= number; i++) {
-    try {
-      document.querySelector("#lemonade_sign" + i).style.display = "inline"
-    }
-    catch(err) {
-      console.log("showSigns", err)
-    }
-  }
-  currentNumber = number
 }
 
 function showLemonade(color) {
@@ -84,7 +36,91 @@ function showLemonade(color) {
       document.querySelector("#glass" + i).style.display = "inline"
     }
   }
+}
 
+function showSigns(number) {
+  if(number > 0) {
+    document.querySelector("#city_map").style.display = "inline"
+  } else {
+    document.querySelector("#city_map").style.display = "none"
+  }
+
+  document.querySelector("#lemonade_signs").style.display = "inline"
+
+  for(i = 1; i <= 8; i++) {
+    document.querySelector("#lemonade_sign" + i).style.display = "none"
+  }
+
+  for(i = 1; i <= number; i++) {
+    try {
+      document.querySelector("#lemonade_sign" + i).style.display = "inline"
+    }
+    catch(err) {
+      console.log("showSigns", err)
+    }
+  }
+  currentNumber = number
+}
+
+async function showCustomers(number) {
+//  if(number > 0){
+//    await sleep(5000)
+//  }
+  if(number > lastActionNumber){
+    //add more customers after the counter is up
+    switch(number){
+      case 1:
+        minCount=40
+        break;
+      case 2:
+        minCount=30
+        break;
+      case 3:
+        minCount=20
+        break;
+      case 4:
+        minCount=10
+        break;
+      case 5:
+        minCount=5
+        break;
+      case 6:
+        minCount=4
+        break;
+      case 7:
+        minCount=3
+        break;
+      case 8:
+        minCount=2
+        break;
+      default:
+        minCount=2
+    }
+  } else if (number < lastActionNumber){
+    //remove the extra customers right away
+  }
+
+  for(i = 1; i <= 8; i++) {
+    document.querySelector("#customer" + i).style.display = "none"
+    document.querySelector("#sad-customer-face-" + i).style.display = "none"
+  }
+
+
+  for(i = 1; i <= number; i++) {
+    if(number >= 2){
+      try {
+        document.querySelector("#customer" + i).style.display = "inline"
+        if(lemonadeColor == "new-recipe"){
+          document.querySelector("#sad-customer-face-" + i).style.display = "inline"
+        }
+      }
+      catch(err) {
+        console.log("showCustomers", err)
+      }
+    }
+  }
+  lastCount = counter
+  lastActionNumber = number
 }
 
 function siteOnline(online) {
@@ -108,13 +144,14 @@ function pullAPI() {
       var data = JSON.parse(this.response)
 
       console.info("API response", data)
-      if(currentNumber != data.ads){
+      latestNumber = data.ads
+      showLemonade(data.lemonade)
+      if(currentNumber != latestNumber){
         showSigns(data.ads)
         showCustomers(data.ads)
       } else {
         lastCount = counter
       }
-      showLemonade(data.lemonade)
       siteOnline(true)
     } else {
       console.log('Error accessing API')
