b0f9e02d2ecad3bbacdb810c7f25ffe1ad57583d
Josh Collins <jcollins@pivotal.io>
2020-06-22 18:09:45 -0700

    back to starting state

--- sidecar.py
+++ sidecar.py
@@ -7,8 +7,8 @@ sidecar_file_name = os.getenv("SIDECAR_FILE_NAME", "/tmp/sidecar.txt")
 while True:
     f = open(sidecar_file_name, "w")
     #f.write("original-recipe")
-    f.write("new-recipe")
-    # f.write("")
+    # f.write("new-recipe")
+    f.write("")
 
     print("Sidecar output: ", f)
     f.close()
